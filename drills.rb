33f0d75fa14838fe88c6124ad88e8fd69bb3afc6
ricarellano <rarellanob@gmail.com>
2017-01-20 16:33:07 -0800

    problems with hash

--- drills.rb
+++ drills.rb
@@ -3,15 +3,26 @@
 #########################
 #say_hello
   # returns 'hello'
-
+def say_hello()
+  'hello'
+end
 #echo
   # echoes (returns) the input string
-
+def echo(string)
+  string
+end
 #get_fruit
   # returns the fruit that corresponds to the given id
   # does not raise an error when the id argument is missing
   # defaults to 'apple' when no id argument is supplied
-
+def get_fruit(index = nil)
+  fruits= ["apple", "pear"]
+    if index == nil
+      "apple"
+    else
+      fruits[index]
+    end
+end
 #how_many_args
   # accepts any number of arguments without error
   # returns the total number of arguments passed to the function
@@ -27,14 +38,22 @@
 #first_char
   # returns the first letter of the given word
   # lowercases the first letter of the given word
+  def first_char(string)
+    string[0].downcase
+  end
 
 #polly_wanna
   # includes the given word in its return value
   # repeats the given word 3 times
+  def polly_wanna(string)
+    string*3
+  end
 
 #count_chars
   # returns the number of characters in the given string
-
+def count_chars(string)
+  string.length
+end
 #yell
   # convert the given message to uppercase
   # adds an exclamation point to the end of the given message
@@ -80,13 +99,20 @@
 ###########################
 #count_spaces
   # counts the spaces in a given string
+def count_spaces(string)
+  string.count(' ')
+end
 
 #string_lengths
   # converts a given list of strings to a list of string lengths
-
+def string_lengths(strings)
+  strings.map {|string| string.length }
+end
 #remove_falsy_values
   # filters out falsy values from the input array
-
+def remove_falsy_values(string)
+  string.select { |item| !!item }
+end
 #exclude_last
   # removes the last item from an input array
   # removes the last item from an input string
@@ -118,6 +144,15 @@
 #count_to
   # returns an array containing every integer from 0 to the input n
   # rounds off decimals
+  def count_to(n)
+    n = n.to_i
+    if n >= 0
+      0..(n).to_a
+    else
+      0.downto(n).to_a
+    end
+  end
+
 
 #is_integer?
   # returns true for Fixnums and Bignums (whole number or 'integer' types)
@@ -125,12 +160,23 @@
   # returns false for non-integer decimals
   # returns false for Float::NAN
   # returns false for non-numbers
+def is_integer?(n)
+  n.class == Fixnum || n.class == Bignum ||  ( n.is_a?(Float) && !n.nan? && n.to_i == n )
+end
+
 
 #is_prime?
   # returns false for non-integer decimals
   # returns false for numbers less than or equal to 1
   # returns false for numbers divisible by anything but 1 and themselves
   # returns true for prime numbers
+  def is_prime?(n)
+    if  !is_integer?(n)  || n <= 1
+      false
+    end
+
+
+  end
 
 #primes_less_than
   # returns an empty array if there are no primes below num
@@ -152,7 +198,9 @@
   # returns a hash
   # counts how many times each character appears in the input string
   # ignores case
-
+def character_count()
+  char_counts={}
+end
 #word_count
   # returns a hash
   # counts how many times a word appears in the input string
